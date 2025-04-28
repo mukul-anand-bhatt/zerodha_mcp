f38f6680b37469137723804968073239ac636537
Mukul Anand Bhatt <onlyforwork941@gmail.com>
2025-04-28 19:04:24 +0530

    access token fixed

--- index.ts
+++ index.ts
@@ -1,59 +1,63 @@
 
 
 
-import { McpServer} from "@modelcontextprotocol/sdk/server/mcp.js";
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import { z } from "zod";
+// import { McpServer} from "@modelcontextprotocol/sdk/server/mcp.js";
+// import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+// import { z } from "zod";
 import {placeOrder} from "./trade"
 
-// Create an MCP server
-const server = new McpServer({
-  name: "Demo",
-  version: "1.0.0"
-});
-
-// Add an addition tool
-server.tool("add-two-numbers",
-  { a: z.number(), b: z.number() },
-  async ({ a, b }) => ({
-    content: [{ type: "text", text: String(a + b) }]
-  })
-);
-
-server.tool("factorial",
-    {a: z.number()},
-    async({ a })=> {
-        let ans = 1;
-        for(let i=2;i<=a;i++){
-            ans*=i;
-        }
-        return {
-            content:[{type:"text",text:String(ans)}]
-        }
-    }
-);
+placeOrder("HCL-INSYS",1,"BUY")
 
 
-server.tool("Buy-stock", 
-    {stock: z.string(), qty: z.number()},
-    async ({stock,qty})=>{
-        placeOrder(stock, qty,"BUY");
-        return {
-            content:[{type:"text", text:"Stock has been bought"}]
-        }
-    }
-)
 
-server.tool("Sell-stock",
-    {stock: z.string(), qty: z.number()},
-    async ({stock, qty}) => {
-        placeOrder(stock, qty, "SELL");
-        return {
-            content:[{type:"text",text:"Stock has been bought"}]
-        }
-    }
-)
+// // Create an MCP server
+// const server = new McpServer({
+//   name: "Demo",
+//   version: "1.0.0"
+// });
 
-// Start receiving messages on stdin and sending messages on stdout
-const transport = new StdioServerTransport();
-await server.connect(transport);
+// // Add an addition tool
+// server.tool("add-two-numbers",
+//   { a: z.number(), b: z.number() },
+//   async ({ a, b }) => ({
+//     content: [{ type: "text", text: String(a + b) }]
+//   })
+// );
+
+// server.tool("factorial",
+//     {a: z.number()},
+//     async({ a })=> {
+//         let ans = 1;
+//         for(let i=2;i<=a;i++){
+//             ans*=i;
+//         }
+//         return {
+//             content:[{type:"text",text:String(ans)}]
+//         }
+//     }
+// );
+
+
+// server.tool("Buy-stock",  "this is a function for zerodha trading app, and this function buys a real stock",
+//     {stock: z.string(), qty: z.number()},
+//     async ({stock,qty})=>{
+//         placeOrder(stock, qty,"BUY");
+//         return {
+//             content:[{type:"text", text:"Stock has been bought"}]
+//         }
+//     }
+// )
+
+// server.tool("Sell-stock", "this is a function for zerodha trading app, and this function sells a real stock",
+//     {stock: z.string(), qty: z.number()},
+//     async ({stock, qty}) => {
+//         placeOrder(stock, qty, "SELL");
+//         return {
+//             content:[{type:"text",text:"Stock has been bought"}]
+//         }
+//     }
+// )
+
+// // Start receiving messages on stdin and sending messages on stdout
+// const transport = new StdioServerTransport();
+// await server.connect(transport);
--- trade.ts
+++ trade.ts
@@ -1,22 +1,82 @@
+// import { KiteConnect } from "kiteconnect";
+// import 'dotenv/config';
+// const accesstoken="7w55GedDzeIUSqmRRD4JjlENZ6lxKjwT"
+// const api_key="x5i8c0lauak0eert"
+// const api_secret="nrkri9wp09dezmvx0wtt46z2p9bmvwot"
+
+// const kc = new KiteConnect({ api_key:  api_key});
+
+// // console.log(kc.getLoginURL());
+
+// export async function placeOrder(tradingsymbol: string, quantity: number, type:"BUY" | "SELL") {
+//   try {
+//     kc.setAccessToken(accesstoken);
+//     await kc.placeOrder("regular",{
+//         exchange:"NSE",
+//         tradingsymbol,
+//         transaction_type:type,
+//         quantity,
+//         product:"CNC",
+//         order_type:"MARKET"
+//     });
+//     // console.log("Profile:", profile);
+//   } catch (err) {
+//     console.error("Error getting profile:", err);
+//   }
+// }
+// // Initialize the API calls
+
+
+
+
+
+
+
+
 import { KiteConnect } from "kiteconnect";
-import 'dotenv/config';
+// import 'dotenv/config';
 
-const kc = new KiteConnect({ api_key:  process.env.KITE_API_KEY!});
 
-export async function placeOrder(tradingsymbol: string, quantity: number, type:"BUY" | "SELL") {
-  try {
-    const profile = await kc.placeOrder("regular",{
-        exchange:"NSE",
-        tradingsymbol:"HDFCBANK",
-        transaction_type:"BUY",
-        quantity:1,
-        product:"CNC",
-        order_type:"MARKET"
-    });
-    console.log("Profile:", profile);
-  } catch (err) {
-    console.error("Error getting profile:", err);
+const kc = new KiteConnect({ api_key:  api_key});
+
+console.log(kc.getLoginURL());
+
+async function init(){
+  try{
+    // await generateSession();
+    kc.setAccessToken(accesstoken);
+    await getProfile();
+  }catch (err){
+    console.log(err);
   }
 }
+
+// async function generateSession() {
+//   try{
+//     const response  = await kc.generateSession(requesttoken,api_secret);
+//     console.log(response.access_token);
+//     kc.setAccessToken(response.access_token);
+//     console.log("session genrated:", response);
+//   }catch (err){
+//     console.log("error genrating session", err);
+//   }
+// }
+
+async function getProfile(){
+  try{
+    const profile = await kc.placeOrder("regular",{
+      exchange:"BSE",
+      tradingsymbol:"HCL-INSYS",
+      transaction_type:"BUY",
+      quantity:1,
+      product:"CNC",
+      order_type:"MARKET"
+    });
+    console.log("Profile:", profile);
+  }catch(err){
+    console.log("Error getting Profile", err);
+  }
+}
+init()
 // Initialize the API calls
 
